056755c434132bfa8b3621d1a2fea4839a751889
Arvind Raju <arvindraju@Arvinds-MBP.lan>
2024-11-16 13:50:40 -0500

    u

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const AuthRouter = require('./Routes/AuthRouter');
-const ExpenseRouter = require('./Routes/ExpenseRouter');
-const { router: PlaidRouter, plaidHealthCheck } = require('./Routes/plaidRouter'); // Import the Plaid router and health check
-const ensureAuthenticated = require('./Middlewares/Auth');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AuthRouter = require("./Routes/AuthRouter");
+const ExpenseRouter = require("./Routes/ExpenseRouter");
+const {
+  router: PlaidRouter,
+  plaidHealthCheck,
+} = require("./Routes/plaidRouter"); // Import the Plaid router and health check
+const ensureAuthenticated = require("./Middlewares/Auth");
 
-require('dotenv').config();
-require('./Models/db');
+require("dotenv").config();
+require("./Models/db");
 const PORT = process.env.PORT || 8080;
 
 // Health Check for Plaid on Startup
 plaidHealthCheck();
 
+// Allow only specific origins
+const allowedOrigins = [
+  "http://localhost:3000", // Development
+  "https://fin-track-lh21.vercel.app", // Frontend in production
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
 app.use(bodyParser.json());
-app.use(cors());
-app.use('/auth', AuthRouter);
-app.use('/expenses', ensureAuthenticated, ExpenseRouter);
-app.use('/plaid', PlaidRouter); // Use the Plaid router
+app.use(cors(corsOptions));
+app.use("/auth", AuthRouter);
+app.use("/expenses", ensureAuthenticated, ExpenseRouter);
+app.use("/plaid", PlaidRouter); // Use the Plaid router
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
